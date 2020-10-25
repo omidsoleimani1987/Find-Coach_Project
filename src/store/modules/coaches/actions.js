6652f47dc92df2e64589692b2fffd04aad89c99c
omidsoleimani <o.soleimani.ie@gmail.com>
2020-10-25 09:18:59 +0100

    finished with fetching from api and add to vuex

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -24,10 +24,37 @@ export default {
     if (!response.ok) {
       // error handling
     }
-
     context.commit('addCoach', {
       ...coachData,
       id: userId
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://find-coach-app-48d60.firebaseio.com/coaches.json`
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // error handling
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas
+      };
+
+      coaches.push(coach);
+
+      context.commit('setCoaches', coaches);
+    }
   }
 };
