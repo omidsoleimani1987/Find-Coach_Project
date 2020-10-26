7dc223f7efbe1cb106c16ac3bae5239c09737bb1
omidsoleimani <o.soleimani.ie@gmail.com>
2020-10-26 21:16:25 +0100

    adding auto logout

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,3 +1,5 @@
+let timer;
+
 export default {
   async login(context, payload) {
     return context.dispatch('auth', {
@@ -41,29 +43,54 @@ export default {
     localStorage.setItem('token', responseData.idToken);
     localStorage.setItem('userId', responseData.localId);
 
+    //setting the expiration date
+    const expiresIn = +responseData.expiresIn * 1000;
+    const expirationDate = new Date().getTime() + expiresIn;
+    localStorage.setItem('tokenExpiration', expirationDate);
+
+    //set the timer after login to automatically logout after expiry time
+    timer = setTimeout(() => {
+      context.dispatch('logout');
+    }, expiresIn);
+
     context.commit('setUser', {
       token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
+      userId: responseData.localId
     });
   },
   tryLogin(context) {
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    // check how much expiration time is left and then set the timer for it
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    if (expiresIn < 0) {
+      return;
+    }
+
+    timer = setTimeout(() => {
+      context.dispatch('logout');
+    }, expiresIn);
 
     if (token && userId) {
       context.commit('setUser', {
         token: token,
-        userId: userId,
-        tokenExpiration: null
+        userId: userId
       });
     }
   },
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+
+    clearTimeout(timer);
+
     context.commit('setUser', {
       token: null,
-      userId: null,
-      tokenExpiration: null
+      userId: null
     });
   }
 };
