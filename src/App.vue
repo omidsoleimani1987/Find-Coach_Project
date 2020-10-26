8ce41a2015ed93f028b3260e5e7dd77b2bbd084d
omidsoleimani <o.soleimani.ie@gmail.com>
2020-10-26 20:49:55 +0100

    adding auto login after reload the browser

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,9 @@ import TheHeader from './components/layout/TheHeader.vue';
 export default {
   components: {
     TheHeader
+  },
+  created() {
+    this.$store.dispatch('tryLogin');
   }
 };
 </script>
