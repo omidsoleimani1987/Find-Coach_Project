356cc1087b96ab0ba5ad0638d8b098a7cb4535ad
omidsoleimani <o.soleimani.ie@gmail.com>
2020-10-26 12:23:52 +0100

    adding not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page Not Found</h2>
+      <p>
+        this page could not be found - maybe check out all our
+        <router-link to="/coaches">Coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
