6d2fe725dcea16195f680695f72bafe30dc4565b
omidsoleimani <o.soleimani.ie@gmail.com>
2020-10-26 13:39:54 +0100

    adding signup page and prepare vuex for authentication

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,96 @@
+<template>
+  <base-card>
+    <form @submit.prevent="submitForm">
+      <div class="form-control">
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model.trim="email" />
+      </div>
+      <div class="form-control">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model.trim="password" />
+      </div>
+      <p v-if="!formIsValid">
+        please enter a valid an email and password (must be six characters long)
+      </p>
+      <base-button>{{ submitButtonCaption }}</base-button>
+      <base-button type="button" mode="flat" @click="switchAuthMode">{{
+        switchModeButtonCaption
+      }}</base-button>
+    </form>
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      formIsValid: true,
+      mode: 'login'
+    };
+  },
+  computed: {
+    submitButtonCaption() {
+      return this.mode === 'login' ? 'Login' : 'Signup';
+    },
+    switchModeButtonCaption() {
+      return this.mode === 'login' ? 'Signup instead' : 'Login instead';
+    }
+  },
+  methods: {
+    submitForm() {
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.password.length < 6
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+      // send http request
+    },
+    switchAuthMode() {
+      if (this.mode === 'login') {
+        this.mode = 'signup';
+      } else {
+        this.mode = 'login';
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+</style>
